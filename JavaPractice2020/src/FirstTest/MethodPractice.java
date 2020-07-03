121965c8460eb875fcc46d3c0df7bba79cd1d9d1
msr494 <msr494@yahoo.com>
2020-07-02 20:35:51 -0500

    2nd commmit of 07022020

--- JavaPractice2020/src/FirstTest/MethodPractice.java
+++ JavaPractice2020/src/FirstTest/MethodPractice.java
@@ -1,13 +1,36 @@
 package FirstTest;
 
+
 public class MethodPractice {
-	
+/*
 	static void practiceMethod() {
 		System.out.println("this is a test msg");
 	}
 
-	public static void main(String[] args) {
-practiceMethod();
-	}
 
+	static void myName (String fname, int age) {
+		System.out.println( fname + " Rahman" + " is " + age );
+		
+	}	
+	
+	
+	static int practiceMethod(int x) {
+		return 10 + x;
+	}
+	*/
+	
+	static void checkAge (int age) {
+	if (age < 15) {
+System.out.println("access denied - you are not welcome");
+	}
+	else {
+		System.out.println("your access is granted");
+	}
+	}
+		
+	public static void main(String[] args) {
+		checkAge(19);
+	}
+	
+	
 }
